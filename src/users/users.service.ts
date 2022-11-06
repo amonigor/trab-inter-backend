48f2461c299b3b9caff5f5598d902d9a1dc0e5cc
Igor Amon <48227869+amonigor@users.noreply.github.com>
2022-11-06 18:16:42 -0300

    finished user controller/service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,70 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(private prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const passEncrypted = await bcrypt.hash(createUserDto.password, 10);
+    const data = {
+      name: createUserDto.name,
+      email: createUserDto.email,
+      username: createUserDto.username,
+      password: passEncrypted,
+    };
+
+    return this.prisma.user.create({
+      data,
+      select: {
+        id: true,
+        username: true,
+        name: true,
+        email: true,
+      },
+    });
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.user.findMany({
+      select: {
+        id: true,
+        username: true,
+        name: true,
+        email: true,
+      },
+    });
   }
 
   findOne(id: string) {
-    return `This action returns a #${id} user`;
+    return this.prisma.user.findUnique({
+      where: { id },
+      select: {
+        id: true,
+        username: true,
+        name: true,
+        email: true,
+      },
+    });
   }
 
   update(id: string, updateUserDto: UpdateUserDto) {
-    return [id, updateUserDto];
+    return this.prisma.user.update({
+      where: { id },
+      data: updateUserDto,
+      select: {
+        id: true,
+        username: true,
+        name: true,
+        email: true,
+      },
+    });
   }
 
   remove(id: string) {
-    return `This action removes a #${id} user`;
+    return this.prisma.user.delete({ where: { id } });
   }
 }
