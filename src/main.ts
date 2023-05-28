b06f6885a83b73c3104e40dc2345ddad533ae8e5
Igor Amon <48227869+amonigor@users.noreply.github.com>
2023-05-28 01:49:24 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  //app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
