37369320c37acba31bdf5c1b91c51b9aafaebc52
Igor Amon <48227869+amonigor@users.noreply.github.com>
2023-05-28 01:47:15 -0300

    testing cors error

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors();
+  //app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
