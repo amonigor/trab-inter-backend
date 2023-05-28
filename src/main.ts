0c1c0d0cacefdea536aec0b1e2269fe093307c40
Igor Amon <48227869+amonigor@users.noreply.github.com>
2023-05-28 01:56:37 -0300

    fixing cors issues

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
-    origin: '*',
-    preflightContinue: true,
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
   });
   await app.listen(3000);
 }
