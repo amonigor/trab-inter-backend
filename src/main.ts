1db3b3b1156429ee0c8e478cec90de74dd4acd70
Igor Amon <48227869+amonigor@users.noreply.github.com>
2023-05-28 13:08:21 -0300

    fix cors error (hopefully)

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,23 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'OPTIONS', 'PATCH', 'DELETE', 'POST', 'PUT'],
+    allowedHeaders: [
+      'X-CSRF-Token',
+      'X-Requested-With',
+      'Accept',
+      'Accept-Version',
+      'Content-Length',
+      'Content-MD5',
+      'Content-Type',
+      'Date',
+      'X-Api-Version',
+      'Authorization',
+    ],
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
