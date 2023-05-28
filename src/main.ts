11ea273fc43346bbbea36940862b7039d077a2d6
Igor Amon <48227869+amonigor@users.noreply.github.com>
2023-05-28 02:11:40 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
-    "origin": "https://primeiros-passos-frontend.vercel.app/",
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": true,
-    "optionsSuccessStatus": 204
+    origin: [
+      'https://primeiros-passos-frontend.vercel.app/',
+      'http://localhost:3000',
+      '*',
+    ],
+    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+    allowedHeaders: [
+      'Accept',
+      'Content-Type',
+      'Authorization',
+    ]
   });
   await app.listen(3000);
 }
