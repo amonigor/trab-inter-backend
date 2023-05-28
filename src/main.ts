9754795bcd1f9627a46a03c7d74483ee29dc389b
Igor Amon <48227869+amonigor@users.noreply.github.com>
2023-05-28 02:06:04 -0300

    fixing cors issues

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
-    "origin": "*",
+    "origin": "https://primeiros-passos-frontend.vercel.app/",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": true,
     "optionsSuccessStatus": 204
