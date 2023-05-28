0650e4a0f9cd66b755d02a649206e509eb3ee86f
Igor Amon <48227869+amonigor@users.noreply.github.com>
2023-05-28 02:15:41 -0300

    reset cors config

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors({
-    origin: [
-      'https://primeiros-passos-frontend.vercel.app/',
-      'http://localhost:3000',
-      '*',
-    ],
-    methods: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
-    preflightContinue: true,
-    optionsSuccessStatus: 204,
-    credentials: true,
-    allowedHeaders: [
-      'Accept',
-      'Content-Type',
-      'Authorization',
-    ]
-  });
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
